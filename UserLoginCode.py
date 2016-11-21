c11b4e50d93e6f74443e6e8792de02a02746109b
AriGlenn <ariisawesome22@gmail.com>
2016-11-21 10:58:10 -0800

    Add Login function with recovery email

--- UserLoginCode.py
+++ UserLoginCode.py
@@ -1,12 +1,32 @@
 
 
+import smtplib
+
+server = smtplib.SMTP('smtp.gmail.com', 587) #port 465 or 587
+server.ehlo()
+server.starttls()
+server.ehlo()
+server.login('logmeinpassrecovery@gmail.com','logmeinmail')
+
+message = '  '
+
+"""
+add check to make sure accounts.txt has been creqated before Login
+
+add readme myFile
+
+add logout myFile
+
+add diff email
+
+"""
 
 def Register():
 	print('Username and Passwords must only contain letters and number')
 	Username = input('Create a Username: ')
 	Password = input('Create a Password: ')
 	CheckPassword = input('Please re-type your Password: ')
-
+	receiver = input ('Please enter a recovery email: ')
 	if CheckPassword != Password:
 		print('Error: Not the same password' + '\n' + 'Please Try Again'  + '\n')
 
@@ -15,11 +35,29 @@ def Register():
 
 
 
-
-
 def Login():
+	loggedin = False
 	UsernameLogin = input('Enter a Username: ')
 	PasswordLogin = input('Enter a Password: ')
+
+	accounts = open('accounts.txt', 'r')
+	accounts = accounts.read()
+	accounts = accounts.split()
+	for x in range(int(len(accounts)/2)):
+		if accounts[x] == UsernameLogin and accounts[x+1] == PasswordLogin:
+			loggedin = True
+
+
+	if loggedin == True:
+		print('Welcome user ' + UsernameLogin + '!')
+	else:
+		print('Incorrect password or username: Please Try Again')
+		reset = input('Forgot Password? Press 1 to reset: ')
+		if reset == '1':
+			receiver = input('Enter your recovery email: ')
+			server.sendmail('logmeinpassrecovery@gmail.com', receiver, message)
+			print('You will be receiveing an email shortly...')
+
 
 
 def LogOut():
